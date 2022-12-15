0bef54ae23011483c7134bdbcbe8324f6694cdaf
Wardo82 <edu_andres@hotmail.es>
2022-12-15 21:13:32 +0100

    Cambios en la ALU identificando los campos de la instrucción

--- src/3_decoder.vhd
+++ src/3_decoder.vhd
@@ -23,53 +23,93 @@ architecture behavioral of decoder is
     signal is_j_instruction: std_logic;
     signal is_r_instruction: std_logic;
     signal is_s_instruction: std_logic;
-    signal opcode: std_logic_vector(4 downto 0);
+    signal signature: std_logic_vector(4 downto 0);
+
+    signal funct3: std_logic_vector(3 downto 0);
+    signal rs1   : std_logic_vector(5 downto 0);
+    signal rs2   : std_logic_vector(5 downto 0);
+    signal rd    : std_logic_vector(5 downto 0);
+    signal opcode: std_logic_vector(6 downto 0);
+
+    signal funct3_valid: std_logic;
+    signal rs1_valid   : std_logic;
+    signal rs2_valid   : std_logic;
+    signal rd_valid    : std_logic;
+    signal imm_valid   : std_logic;
+
+    signal imm         : std_logic_vector(31 downto 0);
+
 begin
-    -- Primero: Decode Logic: Instruction Type
-    
     process(i_clk, i_reset)
     begin
         if rising_edge(i_clk) then
             if i_reset = '1' then
                 
             else
-                opcode <= i_instruction(6 downto 2);
-                if (opcode = "0X101") then
+                -- Primero: Decode Logic: Instruction Type
+                signature <= i_instruction(6 downto 2);
+                if (signature = "0X101") then
                     is_u_instruction <= '1';
                 else
                     is_u_instruction <= '0';
                 end if;
-                if (opcode = "0000X" or opcode = "001X0" or opcode = "11001") then
+                if (signature = "0000X" or signature = "001X0" or signature = "11001") then
                     is_i_instruction <= '1';
                 else 
                     is_i_instruction <= '0';
                 end if;
-                if (opcode = "11000") then
+                if (signature = "11000") then
                     is_b_instruction <= '1';
                 else 
                     is_b_instruction <= '0';
                 end if;
-                if (opcode = "11011") then
+                if (signature = "11011") then
                     is_j_instruction <= '1';
                 else 
                     is_j_instruction <= '0';
                 end if;
-                if (opcode = "01011" or opcode = "011X0" or opcode = "10100") then
+                if (signature = "01011" or signature = "011X0" or signature = "10100") then
                     is_r_instruction <= '1';
                 else 
                     is_r_instruction <= '0';
                 end if;
-                if (opcode = "0100X") then
+                if (signature = "0100X") then
                     is_s_instruction <= '1'; 
                 else 
                     is_s_instruction <= '0';
                 end if;
+                
+                -- Segundo: Decode Logic: Instruction Fields
+                funct3 <= i_instruction(14 downto 12);
+                rs1 <= i_instruction(19 downto 15);
+                rs2 <= i_instruction(24 downto 20);
+                rd <= i_instruction(11 downto 7);
+                opcode <= i_instruction(6 downto 0);
+
+                funct3_valid <= is_r_instruction or is_i_instruction or is_s_instruction or is_b_instruction;
+                rs1_valid    <= is_r_instruction or is_i_instruction or is_s_instruction or is_b_instruction;
+                rs2_valid    <= is_r_instruction or is_s_instruction or is_b_instruction;
+                rd_valid     <= is_r_instruction or is_i_instruction or is_u_instruction or is_j_instruction;
+                imm_valid    <= not is_r_instruction;
+            
+                if (is_i_instruction = '1') then
+                    imm <= (31 downto 11 => i_instruction(31)) & i_instruction(30 downto 20);
+                elsif (is_s_instruction = '1') then
+                    imm <= (31 downto 11 => i_instruction(31)) & i_instruction(30 downto 25) & i_instruction(11 downto 7);
+                elsif (is_b_instruction = '1') then
+                    imm <= (31 downto 12 => i_instruction(31)) & i_instruction(7) & i_instruction(30 downto 25) & i_instruction(11 downto 8) & '0';   
+                elsif  (is_u_instruction = '1') then
+                    imm <= i_instruction(31 downto 12) & (11 downto 0 => '0');           
+                elsif  (is_j_instruction = '1') then
+                    imm <= (31 downto 10 => i_instruction(31)) & i_instruction(19 downto 12) & i_instruction(20) & i_instruction(30 downto 21) & '0';
+                else
+                    imm <= (others => '0');
+                end if;
+                -- Tercero: Decode Logic: Instruction (see image with subset of instructions in red)
 
             end if;
+
         end if;
     end process ; --
-
-    -- Segundo: Decode Logic: Instruction Fields
-    -- Tercero: Decode Logic: Instruction (see image with subset of instructions in red)
     
 end behavioral ; -- behavioral
